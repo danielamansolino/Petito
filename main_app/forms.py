d67db8d22e04f86f6f684ed3b7f07aa682b3600b
Alice Stancu <alice.stancu@gmail.com>
2023-06-07 17:04:42 -0400

    multiple views create

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,9 +1,31 @@
 from django.forms import ModelForm
-from .models import Feeding
+from .models import Feeding, Cleaning, Loving, Moving, Sleeping
 
 class FeedingForm(ModelForm):
   class Meta:
     model = Feeding
     fields = ['food']
     
+class CleaningForm(ModelForm):
+  class Meta:
+    model = Cleaning
+    fields = ['clean']
+    
 
+class LovingForm(ModelForm):
+  class Meta:
+    model = Loving
+    fields = ['love']
+    
+
+class MovingForm(ModelForm):
+  class Meta:
+    model = Moving
+    fields = ['move']
+    
+
+class SleepingForm(ModelForm):
+  class Meta:
+    model = Sleeping
+    fields = ['sleep']
+    
--- main_app/views.py
+++ main_app/views.py
@@ -4,8 +4,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Pet, Feeding
-from .forms import FeedingForm
+from .models import Pet
+from .forms import FeedingForm, CleaningForm, LovingForm, MovingForm, SleepingForm
 
 # Create your views here.
 
@@ -65,11 +65,85 @@ def add_feeding(request, pet_id):
         new_feeding = form.save(commit=False)
         new_feeding.pet_id = pet_id
         new_feeding.save()
-    return redirect('tasks', pet_id = pet_id)
+    return redirect('task_feeding', pet_id = pet_id)
+
+@login_required
+def add_cleaning(request, pet_id):
+    form = CleaningForm(request.POST)
+    if form.is_valid():
+        new_cleaning = form.save(commit=False)
+        new_cleaning.pet_id = pet_id
+        new_cleaning.save()
+    return redirect('task_cleaning', pet_id = pet_id)
+
+@login_required
+def add_loving(request, pet_id):
+    form = LovingForm(request.POST)
+    if form.is_valid():
+        new_loving = form.save(commit=False)
+        new_loving.pet_id = pet_id
+        new_loving.save()
+    return redirect('task_loving', pet_id = pet_id)
+
+@login_required
+def add_moving(request, pet_id):
+    form = MovingForm(request.POST)
+    if form.is_valid():
+        new_moving = form.save(commit=False)
+        new_moving.pet_id = pet_id
+        new_moving.save()
+    return redirect('task_moving', pet_id = pet_id)
+
+@login_required
+def add_sleeping(request, pet_id):
+    form = SleepingForm(request.POST)
+    if form.is_valid():
+        new_sleeping = form.save(commit=False)
+        new_sleeping.pet_id = pet_id
+        new_sleeping.save()
+    return redirect('task_sleeping', pet_id = pet_id)
 
 @login_required
 def tasks(request, pet_id):
     pet = Pet.objects.get(id=pet_id)
-    # food = Pet.objects.get(id=pet_id).feeding_set.get(food)
     feeding_form = FeedingForm()
-    return render(request, 'petitos/tasks.html', {'pet':pet, 'feeding_form': feeding_form})
+    cleaning_form = CleaningForm()
+    loving_form = LovingForm()
+    moving_form = MovingForm()
+    sleeping_form = SleepingForm()
+    return render(request, 'petitos/tasks.html', {'pet':pet, 'feeding_form': feeding_form, 'cleaning_form': cleaning_form, 'loving_form': loving_form, 'moving_form': moving_form, 'sleeping_form': sleeping_form })
+
+
+@login_required
+def task_feeding(request, pet_id):
+    pet = Pet.objects.get(id=pet_id)
+    feeding_form = FeedingForm()
+    return render(request, 'petitos/task_feeding.html', {'pet':pet, 'feeding_form': feeding_form})
+
+
+@login_required
+def task_cleaning(request, pet_id):
+    pet = Pet.objects.get(id=pet_id)
+    cleaning_form = CleaningForm()
+    return render(request, 'petitos/task_cleaning.html', {'pet':pet, 'cleaning_form': cleaning_form})
+
+
+@login_required
+def task_loving(request, pet_id):
+    pet = Pet.objects.get(id=pet_id)
+    loving_form = LovingForm()
+    return render(request, 'petitos/task_loving.html', {'pet':pet, 'loving_form': loving_form})
+
+
+@login_required
+def task_moving(request, pet_id):
+    pet = Pet.objects.get(id=pet_id)
+    moving_form = MovingForm()
+    return render(request, 'petitos/task_moving.html', {'pet':pet, 'moving_form': moving_form})
+
+
+@login_required
+def task_sleeping(request, pet_id):
+    pet = Pet.objects.get(id=pet_id)
+    sleeping_form = SleepingForm()
+    return render(request, 'petitos/task_sleeping.html', {'pet':pet, 'sleeping_form': sleeping_form})
