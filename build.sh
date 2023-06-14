832c3080532ffe79bb78e56a1659b3b286f063af
danielamansolino <danielamosqueragranda@gmail.com>
2023-06-14 16:14:29 -0400

    Update for deploy

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-pip3 install --upgrade pip 
-pip3 install --force-reinstall -U django
-pip3 install --force-reinstall -U dj-database-url
+# pip3 install --upgrade pip 
+# pip3 install --force-reinstall -U django
+# pip3 install --force-reinstall -U dj-database-url
 pip3 install -r requirements.txt
 
 python3 manage.py collectstatic --no-input
