c460310e221b66936e41d352885091d8f81ee488
danielamansolino <danielamosqueragranda@gmail.com>
2023-06-14 15:59:12 -0400

    Update for deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 pip3 install -r requirements.txt
-pip3 install --upgrade pippip install --force-reinstall -U setuptools
+pip3 install --upgrade pip 
+pip3 install --force-reinstall -U setuptools
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate
